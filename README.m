9a54a8437dcaa406c45e2c9e1d7e0d7bbc9ab4e7
abrash <rajirasheed39@gmail.com>
2022-05-30 06:44:28 -0700

    task 0

---
+++ README.m
@@ -0,0 +1,17 @@
+pwd-print working directory,ls : display the content list of your current direct
+
+ory,cd ~ : change my directory to home,ls -l : list the content of the directory in long format,ls -la : display current directory conte
+
+nts including the hidden ones use the long format,ls -all -n : Display current directory contents Long format with user and group IDs di
+
+splayed numerically And hidden files,mkdir /tmp/my_first_directory :create a directory inside /tmp, mv /tmp/Betty /tmp/my_first_director
+
+y:move betty,rm -r /tmp /my_first_directory/betty echo pwd-print working directory,ls : display the content list of your current direct
+
+ory,cd ~ : change my directory to home,ls -l : list the content of the directory in long format,ls -la : display current directory conte
+
+nts including the hidden ones use the long format,ls -all -n : Display current directory contents Long format with user and group IDs di
+
+splayed numerically And hidden files,mkdir /tmp/my_first_directory :create a directory inside /tmp, mv /tmp/Betty /tmp/my_first_director
+
+y:move betty,rm -r /tmp /my_first_directory/betty
